57dd0548f788995561885455224e83b5f8d28885
Najaf-Pc <najaf@outlook.com>
2022-03-28 00:59:54 +0500

    My C# Projects

---
+++ myAppFour/myAppFour/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace myAppFour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rbRed = new System.Windows.Forms.RadioButton();
+            this.rbBlue = new System.Windows.Forms.RadioButton();
+            this.rbGreen = new System.Windows.Forms.RadioButton();
+            this.txtInpString = new System.Windows.Forms.TextBox();
+            this.lblOuput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(299, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(383, 284);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_mouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel_mouseMove);
+            // 
+            // rbRed
+            // 
+            this.rbRed.AutoSize = true;
+            this.rbRed.Location = new System.Drawing.Point(49, 44);
+            this.rbRed.Name = "rbRed";
+            this.rbRed.Size = new System.Drawing.Size(45, 17);
+            this.rbRed.TabIndex = 1;
+            this.rbRed.TabStop = true;
+            this.rbRed.Text = "Red";
+            this.rbRed.UseVisualStyleBackColor = true;
+            // 
+            // rbBlue
+            // 
+            this.rbBlue.AutoSize = true;
+            this.rbBlue.Location = new System.Drawing.Point(48, 67);
+            this.rbBlue.Name = "rbBlue";
+            this.rbBlue.Size = new System.Drawing.Size(46, 17);
+            this.rbBlue.TabIndex = 2;
+            this.rbBlue.TabStop = true;
+            this.rbBlue.Text = "Blue";
+            this.rbBlue.UseVisualStyleBackColor = true;
+            // 
+            // rbGreen
+            // 
+            this.rbGreen.AutoSize = true;
+            this.rbGreen.Location = new System.Drawing.Point(49, 90);
+            this.rbGreen.Name = "rbGreen";
+            this.rbGreen.Size = new System.Drawing.Size(54, 17);
+            this.rbGreen.TabIndex = 3;
+            this.rbGreen.TabStop = true;
+            this.rbGreen.Text = "Green";
+            this.rbGreen.UseVisualStyleBackColor = true;
+            this.rbGreen.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // txtInpString
+            // 
+            this.txtInpString.AllowDrop = true;
+            this.txtInpString.Location = new System.Drawing.Point(49, 132);
+            this.txtInpString.Name = "txtInpString";
+            this.txtInpString.Size = new System.Drawing.Size(100, 20);
+            this.txtInpString.TabIndex = 4;
+            this.txtInpString.TextChanged += new System.EventHandler(this.txtInpString_TextChanged);
+            // 
+            // lblOuput
+            // 
+            this.lblOuput.AutoSize = true;
+            this.lblOuput.Location = new System.Drawing.Point(49, 159);
+            this.lblOuput.Name = "lblOuput";
+            this.lblOuput.Size = new System.Drawing.Size(0, 13);
+            this.lblOuput.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblOuput);
+            this.Controls.Add(this.txtInpString);
+            this.Controls.Add(this.rbGreen);
+            this.Controls.Add(this.rbBlue);
+            this.Controls.Add(this.rbRed);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rbRed;
+        private System.Windows.Forms.RadioButton rbBlue;
+        private System.Windows.Forms.RadioButton rbGreen;
+        private System.Windows.Forms.TextBox txtInpString;
+        private System.Windows.Forms.Label lblOuput;
+    }
+}
+
---
+++ myAppFour/myAppFour/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myAppFour
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Boolean shouldPaint = false;
+        String InpStr;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            shouldPaint = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            shouldPaint = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (shouldPaint)
+            {
+                using (Graphics graphics = CreateGraphics())
+                {
+                    graphics.FillEllipse(new SolidBrush(Color.Blue), e.X, e.Y, 4, 4);
+                }
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel_mouseDown(object sender, MouseEventArgs e)
+        {
+            shouldPaint = true;
+        }
+
+        private void panel_mouseMove(object sender, MouseEventArgs e)
+        {
+            if (shouldPaint)
+            {
+                using (Graphics graphics = CreateGraphics())
+                {
+                    if (rbRed.Checked)
+                    {
+                        graphics.FillEllipse(new SolidBrush(Color.Red), e.X, e.Y, 4, 4);
+                    }
+                }
+            }
+        }
+        int val = 0;
+        private void txtInpString_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
